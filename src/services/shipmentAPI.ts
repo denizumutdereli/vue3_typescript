bb6fb7a579cfe1ee484be41dec6f9fa477789765
Deniz Umut Dereli <197686+denizumutdereli@users.noreply.github.com>
2022-01-28 00:29:37 +0300

    Delete shipmentAPI.ts

--- src/services/shipmentAPI.ts
+++
@@ -1,8 +0,0 @@
-import { TopLevel,Result } from "../types/shipmentContainer.interface.";
- 
-export const shipmentSearch = async (search: string): Promise<TopLevel> =>
-  await fetch(
-    `http://localhost:3004/${search}`
-    //`http://localhost:3004/data`
-  ).then(val => val.json());
- 
